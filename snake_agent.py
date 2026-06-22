c842acebe9510a16528be77a4357bc7aefea5fd4
cecixiaoyu <3569732864@qq.com>
2026-06-22 16:59:19 +0800

    Update snake_agent.py

--- snake_agent.py
+++ snake_agent.py
@@ -1,1 +1,49 @@
+import torch
+import numpy as np
+import random
+from collections import deque
+from snake_model import Linear_QNet, QTrainer
 
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    def __init__(self, n_games=0, max_explore=80):
+        self.n_game = n_games
+        self.max_explore = max_explore
+        self.epsilon = 0  # randomness
+        self.gamma = 0.9  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train_long_memory(self):
+        if len(self.memory) < BATCH_SIZE:
+            return None
+        mini_sample = random.sample(self.memory, BATCH_SIZE)
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        loss = self.trainer.train_step(states, actions, rewards, next_states, dones)
+        return loss
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state, explore=True):
+        if explore:
+            self.epsilon = max(0.05, 1.0 - (self.n_game / self.max_explore))
+            if random.uniform(0, 1) < self.epsilon:
+                move = random.randint(0, 2)
+                final_move = [0, 0, 0]
+                final_move[move] = 1
+                return final_move
+
+        state0 = torch.tensor(state, dtype=torch.float)
+        prediction = self.model(state0)
+        move = torch.argmax(prediction).item()
+        final_move = [0, 0, 0]
+        final_move[move] = 1
+        return final_move
